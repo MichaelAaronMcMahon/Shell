4788d73ea8db622e9f6bee96e7fb888ac022a36c
Michael A. McMahon <mam1198@cheese.cs.rutgers.edu>
2024-04-04 15:26:51 -0400

    wildcards

--- mysh.c
+++ mysh.c
@@ -24,27 +24,82 @@ int executeCmd(char* buf){
 
     token = strtok(NULL, " \n");
     while(token != NULL && argcount < 14){ // leve space for NULL terminator
-        args[argcount] = malloc(strlen(token) + 1); // +1 for null terminator
-        if (!args[argcount]) {
-            perror("malloc");
-            // free previously allocated memory here
-            return -1;
+        if(strstr(token, "*")){ //checks for * wildcard
+            char* wilddir = malloc(strlen(token)+1);
+            strcpy(wilddir, token);
+            int maxSlash = 0;
+            for(int i=0; i<strlen(wilddir); i++){
+                if (wilddir[i] == '/') maxSlash = i;
+            } //wilddir is the full argument
+            char* wildfile = malloc(strlen(wilddir) - maxSlash + 1);
+            int wildindex = 0;
+            strcpy(wildfile, wilddir+maxSlash+1); //copies file name to wildfile
+            for(int i=0; i<strlen(wildfile); i++){
+                if (wildfile[i]=='*'){
+                    wildindex = i;
+                    break;
+                }
+            }
+            printf("wildindex: %d\n", wildindex);
+            DIR *handle;
+            if(maxSlash != 0){
+                wilddir[maxSlash] = '\0'; //terminates wilddir at the last slash, making it just the subdirectories
+                handle = opendir(wilddir); //opens directory
+            }
+            else{
+                handle = opendir(".");
+            }
+            struct dirent *sd = readdir(handle);
+            while(sd != NULL){ //iterates trough each directory entry in wilddir
+                int match = 0;
+                wildfile[wildindex] = '\0';
+                char* argname = malloc(strlen(sd->d_name) + 1);
+                strcpy(argname, sd->d_name);
+                char argorig = argname[wildindex];
+                if(strlen(argname)+1 >= wildindex) argname[wildindex] = '\0';
+                if(strcmp(argname, wildfile) == 0) match++; //compares post-wildcard parts
+                argname[wildindex] = argorig;
+                wildfile[wildindex] = '*';
+                if(strcmp(wildfile+wildindex+1, argname+(strlen(argname) - wildindex)) == 0) match++; //compares pre-wildcard parts
+                if(match==2){ //add matching file to arglist
+                    args[argcount] = malloc(strlen(argname) + 1); // +1 for null terminator
+                    if (!args[argcount]) {
+                        perror("malloc");
+                        // free previously allocated memory here
+                        return -1;
+                    }
+                    strcpy(args[argcount], argname);
+                    argcount++;
+                }
+                
+                sd = readdir(handle); //iterate to next file in wilddir
+            }
+            token = strtok(NULL, " \n");
         }
-        strcpy(args[argcount], token);
-        argcount++
-        token = strtok(NULL, " \n");
+        if(token != NULL){
+            args[argcount] = malloc(strlen(token) + 1); // +1 for null terminator
+            if (!args[argcount]) {
+                perror("malloc");
+                // free previously allocated memory here
+                return -1;
+            }
+            strcpy(args[argcount], token);
+            argcount++;
+            token = strtok(NULL, " \n");
+        }
+        
     }
     args[argcount] = NULL; // null-termiate the argument list
 
     pid_t child = fork();
     
-    if(chld==0){
+    if(child==0){
         execv(prog1, args);
         perror("execv"); // this only reache if execv fails
         exit(EXIT_FAILURE); // ensures child process exits if execv fails
     }
     else if (child == -1){
-        perror("fork")
+        perror("fork");
     }
     
     int wstatus;
@@ -99,7 +154,5 @@ int main(int argc, char ** argv){
     }
     readInput(input, fd);
     
-    
-
 
 }
